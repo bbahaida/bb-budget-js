4e802ef741eab0b24dadfe1dae179adb156bd461
Brahim Bahaida <bbahieda@gmail.com>
2018-06-28 21:25:11 +0000

    Handle the events and added the item to the Budget Module and the UI Module

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
 //#region BUDGET
 
 var BudgetController = (function(){
-    // some code
+
+    
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals:{
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, dsc, val){
+            var newItem, ID, last;
+
+            last = data.allItems[type][data.allItems[type].length - 1];
+
+            ID = last ? last.id + 1 : 1;
+            
+            if(type === 'exp'){
+                newItem = new Expense(ID,dsc,val);
+            }else if(type === 'inc'){
+                newItem = new Income(ID,dsc,val);
+            }
+            data.allItems[type].push(newItem);
+            data.totals[type]+=val;
+            return newItem;
+        },
+        testing: function(){
+            console.log(data);
+        }
+        
+    };
+
 })();
 
 //#endregion
@@ -15,7 +62,9 @@ var UIController = (function(){
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        addButton: '.add__btn'
+        addButton: '.add__btn',
+        incomesContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
     
     return {
@@ -26,9 +75,56 @@ var UIController = (function(){
                 value: document.querySelector(DOMStrings.inputValue).value
             };
         },
+        addListItem: function(type, obj){
+            var html, newHtml, element;
+
+            if(type === 'inc'){
+                element = DOMStrings.incomesContainer;
+                html = '<div class="item clearfix" id="income-%id%"> \
+                            <div class="item__description">%description%</div> \
+                            <div class="right clearfix"> \
+                                <div class="item__value">+ %value%</div> \
+                                <div class="item__delete"> \
+                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> \
+                                </div> \
+                            </div> \
+                        </div>';
+            }else if(type === 'exp'){
+                element = DOMStrings.expensesContainer;                
+                html = '<div class="item clearfix" id="expense-%id%"> \
+                            <div class="item__description">%description%</div> \
+                            <div class="right clearfix"> \
+                                <div class="item__value">- %value%</div> \
+                                <div class="item__percentage">21%</div> \
+                                <div class="item__delete"> \
+                                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> \
+                                </div> \
+                            </div> \
+                        </div>';
+            }
+
+            newHtml = html.replace('%id%',obj.id);
+            newHtml = newHtml.replace('%description%',obj.description);
+            newHtml = newHtml.replace('%value%',obj.value);
+            
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+        },
+        
+        clearFilds: function(){
+            var fields, fieldsArray;
+            fields = document.querySelectorAll(DOMStrings.inputDescription+', '+DOMStrings.inputValue);
+            fieldsArray = Array.prototype.slice.call(fields);
+
+            fieldsArray.forEach(function(element){
+                element.value = '';
+            });
+
+            fieldsArray[0].focus();
+        },
+
         getDOMStrings: function(){
             return DOMStrings;
-        }
+        },
     };
 })();
 
@@ -56,12 +152,18 @@ var AppController = (function(budgetCtrl, UICtrl){
         var input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        var item = budgetCtrl.addItem(input.type, input.description, parseFloat(input.value));
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(input.type, item);
 
-        // 4. Calculate the budget
+        // 4. Clear fields
+        UICtrl.clearFilds();
 
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
+        
 
     }
 
@@ -69,6 +171,8 @@ var AppController = (function(budgetCtrl, UICtrl){
       init: function(){
           console.log('App started .....');
           setupEventListeners();
+          
+          //document.querySelector(UICtrl.getDOMStrings().inputDescription).focus();
           
       }  
     };
